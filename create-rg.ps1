a0507ffb783c06a3f8ff6d1697410dd0ab2e0803
Lucian S. <stoicam.lucian@gmail.com>
2026-06-08 15:19:48 +0300

    Initialize create-rg.ps1 parameter block
    Added CmdletBinding and mandatory script parameters for Environment, App, Region, Location and AdditionalTags.

---
+++ create-rg.ps1
@@ -0,0 +1,8 @@
+[CmdletBinding(SupportsShouldProcess = $true)]
+param(
+[Parameter(Mandatory = $true)][string]$Environment,
+[Parameter(Mandatory = $true)][string]$App,
+[Parameter(Mandatory = $true)][string]$Region,
+[Parameter(Mandatory = $true)][string]$location,
+[Parameter(Mandatory = $false)][hashtable]$AdditionalTags
+)
