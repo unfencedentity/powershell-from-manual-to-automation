6a1a6be6b77b144008f2a1e9c29708e3b33d6df8
Lucian S. <stoicam.lucian@gmail.com>
2026-07-24 19:56:48 +0300

    Enhance exercise.ps1 with variable exercises
    Added exercises for creating and manipulating PowerShell variables.

--- 01-variables/exercise.ps1
+++ 01-variables/exercise.ps1
@@ -1,8 +1,37 @@
-$resource   = "vm"
-$application = "cloud-org"
-$environment = "prod"
-$region      = "neu"
+<#
+Chapter 01 - Variables
 
-$vmName = "$resource-$application-$environment-$region"
+Complete each exercise using PowerShell variables.
+#>
 
-Write-Host $vmName
+
+# Exercise 1
+# Create variables for:
+# - environment: dev
+# - application: core
+# - region: weu
+
+# TODO: Create the variables here.
+
+
+# Exercise 2
+# Build an Azure resource group name using this format:
+#
+# rg-<application>-<environment>-<region>
+
+# TODO: Build and return the resource group name.
+
+
+# Exercise 3
+# Produce one string using this format:
+#
+# Deploying <application> to <environment> in <region>
+
+# TODO: Write the interpolated string here.
+
+
+# Exercise 4
+# Change the environment value from dev to prod.
+# Build the resource group name again.
+
+# TODO: Update the variable and rebuild the name.
